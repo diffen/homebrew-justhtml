9cbdf8d2c2b5755a0dd0e96200d527639f438ca4
Nikhilesh Jasuja <jasuja@diffen.com>
2026-07-10 22:09:37 -0700

    Bump justhtml to v0.1.4

--- Formula/justhtml.rb
+++ Formula/justhtml.rb
@@ -1,8 +1,8 @@
 class Justhtml < Formula
   desc "HTML5 parser CLI with CSS selectors and full html5lib compliance"
   homepage "https://github.com/diffen/justhtml-php"
-  url "https://github.com/diffen/justhtml-php/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "a61b3e229908285bbc7b6693787386b07741b234840b1506e01d97de5a641a68"
+  url "https://github.com/diffen/justhtml-php/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "0674f3d103b6491b046b8ac4a0ed578da5c36dbb573e7d96d4fe043e244cadfc"
   license "MIT"
 
   depends_on "php"
